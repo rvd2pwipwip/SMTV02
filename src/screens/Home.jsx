92cd36dc4e8075173b60fec01b53589dfb3ff5bf
rvd2pwipwip <herve_desrosiers@hotmail.com>
2025-04-21 15:31:39 -0400

    replaced home first local card with library card

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { ChannelCard as LibraryChannelCard } from '@smtv/tv-component-library/dist/index.umd.js';
+import '@smtv/tv-component-library/dist/style.css';
 import ChannelCard from '../components/ChannelCard';
 import Header from '../components/Header';
 import '../styles/App.css';
@@ -12,8 +14,9 @@ function Home() {
     <div className="app">
       <Header title="TV App" />
       <div className="content-swimlane">
-        <ChannelCard 
+        <LibraryChannelCard 
           title="Sample Channel" 
+          thumbnailUrl="https://picsum.photos/300/300"
           onSelect={handleChannelSelect} 
         />
         <ChannelCard 
