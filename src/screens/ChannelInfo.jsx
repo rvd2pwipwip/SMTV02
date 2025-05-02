fd8c125381d3cc9c7649381660cafdb63ebefa9b
Herve Desrosiers <hdesrosiers@mtln4yp2f9w-mac.corp.stingraydigital.com>
2025-05-02 14:08:44 -0400

    removed UMD import to use modern import for ChannelCard

--- src/screens/ChannelInfo.jsx
+++ src/screens/ChannelInfo.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ChannelCard } from '@smtv/tv-component-library/dist/index.umd.js';
+import { ChannelCard } from '@smtv/tv-component-library';
 import Header from '../components/Header';
 import '../styles/App.css';
 
